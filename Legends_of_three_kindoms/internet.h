cc368db7d6f7b1000d900a51bc73b239c4ef5a6c
Libestor <443318033@qq.com>
2022-05-25 16:10:18 +0800

    init_internet

--- Legends_of_three_kindoms/internet.h
+++ Legends_of_three_kindoms/internet.h
@@ -1,1 +1,16 @@
 #pragma once
+
+typedef int STATUS;
+typedef struct SOCKET {
+
+};
+
+typedef  struct MSG {
+	int blood;
+	int card;
+};
+SOCKET* init_socket();
+STATUS connect_port(SOCKET Socket,int port);
+STATUS connect_ip(SOCKET Socket, char ip[], int port);
+STATUS send_msg(SOCKET Socket, MSG msg);
+STATUS get_msg(SOCKET Socket, MSG* msg);
