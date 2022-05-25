2bf25940bd6a7e95d96f0e38febbb5539cbabf0f
Libestor <443318033@qq.com>
2022-05-25 18:07:07 +0800

    new_internet

--- Legends_of_three_kindoms/internet.h
+++ Legends_of_three_kindoms/internet.h
@@ -1,14 +1,12 @@
 #pragma once
-
 typedef int STATUS; //状态信息
-
 typedef  struct MSG {
 	int blood;
 	int enemy;
 	int card;
 };
 STATUS init_connect();
-STATUS connect_port(int port);
-STATUS connect_ip( char ip[], int port);
+STATUS get_ip(char ip[]);//告诉我本地地址
+STATUS connect_ip( char ip[]);
 STATUS send_msg( MSG msg);
 STATUS get_msg( MSG* msg);
