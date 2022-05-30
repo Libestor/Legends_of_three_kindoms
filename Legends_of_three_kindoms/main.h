b8731c60d35ee0fdaf8cf7985e06d4b7e8698633
Libestor <443318033@qq.com>
2022-05-30 17:05:07 +0800

    enum warloads

--- Legends_of_three_kindoms/main.h
+++ Legends_of_three_kindoms/main.h
@@ -1,11 +1,17 @@
 #pragma once
 #include<stdio.h>
 #include<Windows.h>
+#include"skills_cards.h"
+#include"LinkList.h"
 typedef int STATUS;
+enum warloads {
+	zhangfei,
+	dianwei,
+};
 typedef struct USER {
 	char ip[20];
-	int port;
-	//手牌
+	WARLORD wj;
+	Head* shoupai;
 
 };
  enum CS {
@@ -14,4 +20,6 @@ typedef struct USER {
 };
  STATUS init_all(USER* user);
  STATUS GameOver(); //gameover成功就进入新一盘游戏，否则就直接停止程序
+
+
 
