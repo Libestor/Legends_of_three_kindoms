ee2703d813c23f9fcfc941d4ec349e86bbfb29e8
Libestor <443318033@qq.com>
2022-06-08 14:52:58 +0800

    int cur1

--- Legends_of_three_kindoms/skills_cards.h
+++ Legends_of_three_kindoms/skills_cards.h
@@ -23,7 +23,7 @@ typedef struct CARDS
 {
 	int color;//1：红桃 2：黑桃 3：方块 4：梅花
 	int points;
-	int (*skill)(USER* A, USER* B);
+	int (*skill)(USER* A, USER* B, int cur);
 	int name;//卡牌的名字
 }CARDS;
 typedef struct Node// 定义链表节点
@@ -63,24 +63,24 @@ WARLORD* zhangfei();//张飞的初始化
 WARLORD* dianwei();//典韦的初始化
 int dianwei_skill(WARLORD* A, WARLORD* B);
 CARDS* kill();//定义花色定义点数
-int kill_skill(USER* A, USER* B);//杀的功能
+int kill_skill(USER* A, USER* B,int cur);//杀的功能
 CARDS* shan();//定义闪的花色和点数
-int shan_skill(USER* A, USER* B);//闪的功能
+int shan_skill(USER* A, USER* B,int cur);//闪的功能
 CARDS* tao();//定义桃的花色和点数
-int tao_skill(USER* A,USER* B);//桃的功能
+int tao_skill(USER* A,USER* B, int cur);//桃的功能
 WARLORD* search_wujiang();//随机抽取武将
 CARDS* search_pai();//随机抽取手牌
 WARLORD* mustzhangfei();//定向抽取张飞
 WARLORD* mustdianwei();//定向抽取典韦
 CARDS* getcards(int i);//根据传入数据定向返回卡牌
 CARDS* juedou();//定义决斗卡牌的点数和花色
-int juedou_skill(USER* A, USER* B);//定义决斗的功能
+int juedou_skill(USER* A, USER* B, int cur);//定义决斗的功能
 CARDS* nanmanruqin();//定义南蛮入侵的点数和花色
-int nanmanruqin_skill(USER* A, USER* B);//定义南蛮入侵的功能
+int nanmanruqin_skill(USER* A, USER* B, int cur);//定义南蛮入侵的功能
 CARDS* wanjianqifa();//定义万箭齐发的点数和花色
-int wanjianqifa_skill(USER* A, USER* B);//定义万箭齐发的功能
+int wanjianqifa_skill(USER* A, USER* B, int cur);//定义万箭齐发的功能
 int delatenodelocate(Head* head, int n);//指定位置删除节点
 //int (*shanchu)(USER* Our, USER* enemy, int card);
 CARDS* guohechaiqiao();//定义过河拆桥的花色和点数
-int guohechaiqiao_skill(USER* A, USER* B);//定义过河拆桥的功能
+int guohechaiqiao_skill(USER* A, USER* B, int cur);//定义过河拆桥的功能
 CARDS* wuxiekeji();//定义无懈可击的花色和点数
